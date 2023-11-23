6c5b3bde29369e395e79afcc9f3093fefade983a
Jonathan Estabillo <jonathan.estabillo@gmail.com>
2023-11-23 22:59:32 +0000

    Greet Hello First PR

--- src/routes/greet.js
+++ src/routes/greet.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // GET greet endpoint
 router.get('/', function(req, res, next) {
-  res.send('Hello PR!');
+  res.send('Hello First PR!');
 });
 
 module.exports = router;
