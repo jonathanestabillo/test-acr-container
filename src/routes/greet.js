4a665506fc70b3c22d669ad332dd13365dc60daf
Jonathan Estabillo <jonathan.estabillo@gmail.com>
2023-12-02 21:35:09 +0000

    Initial Dev Container Deployment

--- src/routes/greet.js
+++ src/routes/greet.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // GET greet endpoint
 router.get('/', function(req, res, next) {
-  res.send('Create New Image Tag and Delete It on Close');
+  res.send('Initial Image');
 });
 
 module.exports = router;
