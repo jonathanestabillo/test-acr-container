5b331aef8b1bf698c675a9d3b38abe09303a4e0f
Jonathan Estabillo <jonathan.estabillo@gmail.com>
2023-12-05 05:24:16 +0000

    Test App Configuration Value

--- src/routes/greet.js
+++ src/routes/greet.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // GET greet endpoint
 router.get('/', function(req, res, next) {
-  res.send('The Docker Registry Server value is: ' + process.env.DOCKER_REGISTRY_SERVER_URL);
+  res.send('MY_NAME value is: ' + process.env.MY_NAME);
 });
 
 module.exports = router;
