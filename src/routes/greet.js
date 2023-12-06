e7f8f0ed8ca55fec3437cba53bec2d1fe8be3b08
Jonathan Estabillo <jonathan.estabillo@gmail.com>
2023-12-06 02:02:16 +0000

    Test KeyVault

--- src/routes/greet.js
+++ src/routes/greet.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // GET greet endpoint
 router.get('/', function(req, res, next) {
-  res.send('KEYVAULT value is: ' + process.env.TEST_KEYVAULT);
+  res.send('KEYVAULT value is: ' + process.env.TEST_SECRET);
 });
 
 module.exports = router;
