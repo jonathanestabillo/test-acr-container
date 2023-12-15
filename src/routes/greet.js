e1504a1ed33d6f2b1c1508ccc8e2bb23c7c49d49
Jonathan Estabillo <jonathan.estabillo@gmail.com>
2023-12-15 02:14:35 +0000

    Change Database Connectivity

--- src/routes/greet.js
+++ src/routes/greet.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 // GET greet endpoint
 router.get('/', async function(req, res, next) {
-  const DB_NAME = process.env.DB_NAME;
-  const db = req.dbClient.db(DB_NAME);
+  const db = req.dbClient.db(process.env.DB_NAME);
   const collection = db.collection('Users');
   const users = await collection.find().toArray();
   res.send(`Hello ${users[0].firstName} ${users[0].lastName}`);
